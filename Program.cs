7fa97a2526464bb81d3d28c22a227ace63cc3186
ommpatel142 <ommpatel142@gmail.com>
2026-07-10 03:17:30 -0700

    Learning

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using ASPNETCORELEARNING.Data;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MenuContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
